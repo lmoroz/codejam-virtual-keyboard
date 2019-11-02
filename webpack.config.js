72ec2a925141d36f947e888740da93b68b414111
Larisa Morozhnikova <larisa.moroz@gmail.com>
2019-11-03 01:58:48 +0800

    doc: install necessary modules and create files

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  target: 'web',
+  mode: 'development',
+  entry: { app: './src/app.js' },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './build/'),
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),
+    watchContentBase: true,
+    compress: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: false,
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
+  module: {
+  },
+};
