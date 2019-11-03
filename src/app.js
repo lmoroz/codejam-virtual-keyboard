8f5859415f40c7477ad72ff4b70c9bdf51c769cd
Larisa Morozhnikova <larisa.moroz@gmail.com>
2019-11-03 18:36:45 +0800

    feat: dynamically add stylesheet

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
-import keysConfig from './modules/keymap';
-import Key from './modules/Key';
-
-
-
+// import keysConfig from './modules/keymap';
+// import Textbox from './modules/Textbox';
+// import Keyboard from './modules/Keyboard';
+// import Key from './modules/Key';
+const initHTML = () => {
+  const head = document.getElementsByTagName('HEAD')[0];
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.href = 'app.css';
+  head.appendChild(link);
+};
+document.addEventListener('DOMContentLoaded', initHTML);
